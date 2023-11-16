c79118015c874c3f221c0f17a94fab6a74a6511b
Ilya <ilya.yohumming@icloud.com>
2023-11-16 12:22:55 +0600

    add slider and bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,5 @@ var header__nav = document.querySelector(".header__nav");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
   header__nav.classList.toggle("is-active");
+  document.body.classList.toggle("fixed");
 });
